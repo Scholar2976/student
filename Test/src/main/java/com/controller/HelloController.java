fbcaa55e8f65bbc241fcb1cc47dba7e882560827
殷沛瑜 <yinpeiyu@opencode.cloud>
2020-04-08 16:09:15 +0800

    first commit

---
+++ Test/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController 
+{
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String getHello()
+	{
+		System.out.println("hello");
+		return "hello";
+	}
+}
---
+++ Test/src/main/java/com/testApp.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class testApp
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(testApp.class, args);
+	}
+}
